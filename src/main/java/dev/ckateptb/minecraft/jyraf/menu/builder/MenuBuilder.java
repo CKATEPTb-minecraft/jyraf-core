157b0bc885ea6b18b2b14cb00a1c65767d1a574f
CKATEPTb <taksis@ex.ua>
2023-12-19 06:16:03 +0200

    Extract Frame builder

--- src/main/java/dev/ckateptb/minecraft/jyraf/menu/builder/MenuBuilder.java
+++ src/main/java/dev/ckateptb/minecraft/jyraf/menu/builder/MenuBuilder.java
@@ -1,21 +1,13 @@
 package dev.ckateptb.minecraft.jyraf.menu.builder;
 
-import dev.ckateptb.minecraft.jyraf.builder.item.ItemBuilder;
 import dev.ckateptb.minecraft.jyraf.menu.Menu;
 import dev.ckateptb.minecraft.jyraf.menu.chest.ChestMenu;
 import dev.ckateptb.minecraft.jyraf.menu.frame.Frame;
-import dev.ckateptb.minecraft.jyraf.menu.frame.button.ButtonFrame;
-import dev.ckateptb.minecraft.jyraf.menu.frame.conditional.ConditionalFrame;
-import dev.ckateptb.minecraft.jyraf.menu.frame.item.ItemFrame;
-import dev.ckateptb.minecraft.jyraf.menu.frame.pageable.PageableFrame;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -24,7 +16,12 @@ public class MenuBuilder {
         return new ChestBuilder(title, rows);
     }
 
-    public interface Context {
+    public Frame.Builder frame() {
+        return (Context) (slot, frame) -> {
+        };
+    }
+
+    public interface Context extends Frame.Builder {
         void set(int slot, Frame frame);
 
         default void set(int[] slots, Frame frame) {
@@ -52,54 +49,6 @@ public class MenuBuilder {
         default void row(int row, Frame frame) {
             int offset = row * 9;
             this.range(offset - 9, offset - 1, frame);
-        }
-
-        default ItemFrame item(Material material, Consumer<ItemBuilder> consumer) {
-            ItemBuilder builder = new ItemBuilder(material);
-            consumer.accept(builder);
-            return this.item(builder.build());
-        }
-
-        default ItemFrame item(ItemStack stack, Consumer<ItemBuilder> consumer) {
-            ItemBuilder builder = new ItemBuilder(stack);
-            consumer.accept(builder);
-            return this.item(builder.build());
-        }
-
-        default ItemFrame item(ItemStack stack) {
-            ItemFrame.Builder builder = new ItemFrame.Builder();
-            builder.item(stack);
-            return builder.build();
-        }
-
-        default ButtonFrame item(Material material, Consumer<ItemBuilder> consumer, Menu.ClickHandler handler) {
-            ItemBuilder builder = new ItemBuilder(material);
-            consumer.accept(builder);
-            return this.item(builder.build(), handler);
-        }
-
-        default ButtonFrame item(ItemStack stack, Consumer<ItemBuilder> consumer, Menu.ClickHandler handler) {
-            ItemBuilder builder = new ItemBuilder(stack);
-            consumer.accept(builder);
-            return this.item(builder.build(), handler);
-        }
-
-        default ButtonFrame item(ItemStack stack, Menu.ClickHandler handler) {
-            ButtonFrame.Builder builder = new ButtonFrame.Builder();
-            builder.item(stack);
-            builder.addClickHandler(handler);
-            return builder.build();
-        }
-
-        default PageableFrame pagination(int[] slots, Consumer<PageableFrame.Builder> consumer) {
-            PageableFrame.Builder builder = new PageableFrame.Builder(slots);
-            consumer.accept(builder);
-            return builder.build();
-        }
-
-        default ConditionalFrame conditional(Supplier<Boolean> condition, Frame success, Frame failed) {
-            ConditionalFrame.Builder builder = new ConditionalFrame.Builder(condition, success, failed);
-            return builder.build();
         }
     }
 
