3c2edc807cc1fc03b93cc0fa6734667b504ccf53
CKATEPTb <taksis@ex.ua>
2023-12-22 04:52:29 +0200

    Fix ORMLite relocate. Add PersisterInjection and LocalDateTimeType persister

---
+++ src/main/java/dev/ckateptb/minecraft/jyraf/database/types/LocalDateTimeType.java
@@ -0,0 +1,30 @@
+package dev.ckateptb.minecraft.jyraf.database.types;
+
+import com.j256.ormlite.field.FieldType;
+import com.j256.ormlite.field.SqlType;
+import com.j256.ormlite.field.types.LongType;
+
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class LocalDateTimeType extends LongType {
+
+    public LocalDateTimeType() {
+        super(SqlType.LONG, new Class<?>[]{LocalDateTime.class});
+    }
+
+    @Override
+    public Object sqlArgToJava(FieldType fieldType, Object sqlArg, int columnPos) throws SQLException {
+        long value = (Long) super.sqlArgToJava(fieldType, sqlArg, columnPos);
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(value), ZoneId.systemDefault());
+    }
+
+    @Override
+    public Object javaToSqlArg(FieldType fieldType, Object object) {
+        LocalDateTime localDateTime = (LocalDateTime) object;
+        return ZonedDateTime.of(localDateTime, ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+}
