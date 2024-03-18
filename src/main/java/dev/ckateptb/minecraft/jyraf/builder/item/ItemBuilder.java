7df170957d511b97b72282526d1a3160f1cc89cd
hogoshi <hogoshi@yandex.ru>
2024-03-18 23:22:59 +0700

    generics, again.

--- src/main/java/dev/ckateptb/minecraft/jyraf/builder/item/ItemBuilder.java
+++ src/main/java/dev/ckateptb/minecraft/jyraf/builder/item/ItemBuilder.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings({"unchecked"})
-public class ItemBuilder<B extends ItemBuilder<B>> implements Builder<ItemStack> {
+public class ItemBuilder<R extends ItemBuilder<R>> implements Builder<ItemStack> {
 
     protected final ItemStack item;
     protected final ItemMeta meta;
@@ -45,79 +45,79 @@ public class ItemBuilder<B extends ItemBuilder<B>> implements Builder<ItemStack>
         this.meta = this.item.getItemMeta();
     }
 
-    public <K, V> B set(@NonNull NamespacedKey key, @NonNull PersistentDataType<K, V> type, V value) {
-        if (this.meta == null) return (B) this;
+    public <K, V> R setTag(@NonNull NamespacedKey key, @NonNull PersistentDataType<K, V> type, V value) {
+        if (this.meta == null) return (R) this;
         PersistentDataContainer pdc = meta.getPersistentDataContainer();
 
         if (value == null) {
             pdc.remove(key);
-            return (B) this;
+            return (R) this;
         }
         pdc.set(key, type, value);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B name(String name) {
-        if (this.meta == null) return (B) this;
+    public R name(String name) {
+        if (this.meta == null) return (R) this;
         this.meta.displayName(Text.of(name));
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B amount(int amount) {
+    public R amount(int amount) {
         this.item.setAmount(amount);
-        return (B) this;
+        return (R) this;
     }
 
-    public B lore(String... lore) {
+    public R lore(String... lore) {
         return lore(Arrays.asList(lore));
     }
 
-    public B lore(List<String> lore) {
-        if (this.meta == null || lore == null || lore.isEmpty()) return (B) this;
+    public R lore(List<String> lore) {
+        if (this.meta == null || lore == null || lore.isEmpty()) return (R) this;
         this.meta.lore(lore.stream().map(Text::of).toList());
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B color(Color color) {
+    public R color(Color color) {
         return this.durability(color.data());
     }
 
-    public B durability(short durability) {
-        if (!(this.meta instanceof Damageable damageable)) return (B) this;
+    public R durability(short durability) {
+        if (!(this.meta instanceof Damageable damageable)) return (R) this;
         damageable.setDamage(durability);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B enchant(Enchantment enchantment, int level) {
-        if (this.meta == null) return (B) this;
+    public R enchant(Enchantment enchantment, int level) {
+        if (this.meta == null) return (R) this;
         if (this.meta instanceof EnchantmentStorageMeta storageMeta) {
             storageMeta.addStoredEnchant(enchantment, level, true);
         } else {
             this.meta.addEnchant(enchantment, level, true);
         }
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B unenchant(Enchantment... enchantments) {
-        if (this.meta == null) return (B) this;
+    public R unenchant(Enchantment... enchantments) {
+        if (this.meta == null) return (R) this;
         for (Enchantment enchantment : enchantments) {
             this.meta.removeEnchant(enchantment);
         }
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B attribute(Attribute attribute, AttributeModifier modifier) {
+    public R attribute(Attribute attribute, AttributeModifier modifier) {
         return this.attribute(attribute, modifier, true);
     }
 
-    public B attribute(Attribute attribute, AttributeModifier modifier, boolean overwrite) {
-        if (this.meta == null) return (B) this;
+    public R attribute(Attribute attribute, AttributeModifier modifier, boolean overwrite) {
+        if (this.meta == null) return (R) this;
         if (overwrite) {
             Multimap<Attribute, AttributeModifier> attributes = this.meta.getAttributeModifiers();
             AttributeModifier finalModifier = modifier;
@@ -129,45 +129,45 @@ public class ItemBuilder<B extends ItemBuilder<B>> implements Builder<ItemStack>
 
         this.meta.addAttributeModifier(attribute, modifier);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B unattribute(Attribute... attributes) {
-        if (this.meta == null) return (B) this;
+    public R unattribute(Attribute... attributes) {
+        if (this.meta == null) return (R) this;
         for (Attribute attribute : attributes) {
             this.meta.removeAttributeModifier(attribute);
         }
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B flag(ItemFlag... flag) {
-        if (this.meta == null) return (B) this;
+    public R flag(ItemFlag... flag) {
+        if (this.meta == null) return (R) this;
         this.meta.addItemFlags(flag);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B deflag(ItemFlag... flag) {
-        if (this.meta == null) return (B) this;
+    public R deflag(ItemFlag... flag) {
+        if (this.meta == null) return (R) this;
         this.meta.removeItemFlags(flag);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B unbreakable(boolean unbreakable) {
+    public R unbreakable(boolean unbreakable) {
         meta.setUnbreakable(unbreakable);
 
-        return (B) this;
+        return (R) this;
     }
 
-    public B skull(String texture) {
-        if (!(this.meta instanceof SkullMeta skullMeta)) return (B) this;
+    public R skull(String texture) {
+        if (!(this.meta instanceof SkullMeta skullMeta)) return (R) this;
         PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID());
         profile.getProperties().add(new ProfileProperty("textures", texture));
         skullMeta.setPlayerProfile(profile);
 
-        return (B) this;
+        return (R) this;
     }
 
     public ItemStack build() {
